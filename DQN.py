4eb08a8ef96ea5f8453d6d134a8c9badc5ce5fcd
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-03-06 01:56:17 +0530

    modules that will be required to implement

---
+++ DQN.py
@@ -0,0 +1,11 @@
+#modules
+#1) Replay bufffer
+#2) DQN
+#3) DQN with target network
+#4) sampleing from replay buffer
+#5) preprocesing of the input to 84x84x4
+#6) training the network
+#7) testing the network-> reward and preicted Q Vlue estimation
+#8) saving the model
+
+
