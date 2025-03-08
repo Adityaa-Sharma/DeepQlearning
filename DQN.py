acede576490e24571827d52a4e474f0d44f35f07
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-03-08 23:50:44 +0530

    pushing the DQN

--- DQN.py
+++ DQN.py
@@ -42,3 +42,32 @@ class ReplayBuffer:
     def __len__(self):
         return len(self.buffer)
 
+class DQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(DQN, self).__init__()
+        self.input_shape = input_shape
+        self.num_actions = num_actions
+        self.conv1 = nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.fc = nn.Linear(self.feature_size(), 512)
+        self.out = nn.Linear(512, num_actions)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.fc(x.view(x.size(0), -1)))
+        return self.out(x)
+
+    def feature_size(self):
+        return self.conv3(self.conv2(self.conv1(torch.zeros(1, *self.input_shape)))).view(1, -1).size(1)
+
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state = torch.FloatTensor(np.float32(state)).unsqueeze(0)
+            q_value = self.forward(state)
+            action = q_value.max(1)[1].data[0]
+        else:
+            action = random.randrange(self.num_actions)
+        return action
