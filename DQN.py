ef0e72c9b4fedfd1718e64623634e189c3cdc6fb
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-03-17 01:08:12 +0530

    addin the preprocessing code

--- DQN.py
+++ DQN.py
@@ -20,11 +20,13 @@ import torch.nn.functional as F
 import numpy as np
 import random
 import gym
+from configs import ModelConfig
+import cv2
 
 
 class ReplayBuffer:
-    def __init__(self, capacity):
-        self.capacity = capacity
+    def __init__(self):
+        self.capacity = ModelConfig.ReplayBufferCapacity
         self.buffer = []
         self.position = 0
 
@@ -43,15 +45,15 @@ class ReplayBuffer:
         return len(self.buffer)
 
 class DQN(nn.Module):
-    def __init__(self, input_shape, num_actions):
+    def __init__(self, input_shape):
         super(DQN, self).__init__()
         self.input_shape = input_shape
-        self.num_actions = num_actions
+        self.num_actions = ModelConfig.NumActions
         self.conv1 = nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
         self.fc = nn.Linear(self.feature_size(), 512)
-        self.out = nn.Linear(512, num_actions)
+        self.out = nn.Linear(512, self.num_actions)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -61,13 +63,13 @@ class DQN(nn.Module):
         return self.out(x)
 
     def feature_size(self):
-        return self.conv3(self.conv2(self.conv1(torch.zeros(1, *self.input_shape)))).view(1, -1).size(1)
+        return self.conv3(self.conv2(self.conv1(torch.zeros(1, *self.input_shape)))).view(1, -1).size(1) # this will output 3136
 
     def act(self, state, epsilon):
         if random.random() > epsilon:
-            state = torch.FloatTensor(np.float32(state)).unsqueeze(0)
+            state = torch.FloatTensor(np.float32(state)).unsqueeze(0) # adding the batch dimension
             q_value = self.forward(state)
-            action = q_value.max(1)[1].data[0]
+            action = q_value.max(1)[1].item()
         else:
             action = random.randrange(self.num_actions)
         return action
@@ -76,5 +78,14 @@ class DQN(nn.Module):
 def DQN_Target_Network():
     pass
 
+
+def PreProcessing():
+    # This will make the input as 210x160x3 to 84x84x4
+    def preprecess(image):
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        gray = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
+        gray=gray/255.0
+        return torch.FloatTensor(gray).unsqueeze(0)
+        
 
     
--- configs.py
+++ configs.py
@@ -3,7 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class ModelConfig:
-    ReplayBuffer: int = 1000000 # 1mn from the paper
+    ReplayBufferCapacity: int = 1000000 # 1mn from the paper
+    NumActions: int = 4 # Breakout has 4 actions
+    
     
     
     
